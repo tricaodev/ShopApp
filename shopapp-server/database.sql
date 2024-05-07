a6bb175c02c88d0b55bda06324948f8d4cd42f2b
Tri Cao <tricao.dev@gmail.com>
2024-05-07 17:51:06 +0700

    Cấu trúc lại thư mục dự án

---
+++ shopapp-server/database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE shopapp;
+USE shopapp;
+
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(100) DEFAULT '',
+    phone_number VARCHAR(10) NOT NULL,
+    address VARCHAR(200) DEFAULT '',
+    password VARCHAR(100) NOT NULL DEFAULT '',
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_active TINYINT(1) DEFAULT 1,
+    date_of_birth DATE,
+    facebook_account_id INT DEFAULT 0,
+    google_account_id INT DEFAULT 0,
+);
