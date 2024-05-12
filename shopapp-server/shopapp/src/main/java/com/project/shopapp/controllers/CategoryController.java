d92e7aa2411e6b84aab1b05c9530ece48ced34ee
Tri Cao <tricao.dev@gmail.com>
2024-05-13 01:18:52 +0700

    update code

--- shopapp-server/shopapp/src/main/java/com/project/shopapp/controllers/CategoryController.java
+++ shopapp-server/shopapp/src/main/java/com/project/shopapp/controllers/CategoryController.java
@@ -5,9 +5,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
--- shopapp-server/shopapp/src/main/java/com/project/shopapp/dtos/ProductDto.java
+++ shopapp-server/shopapp/src/main/java/com/project/shopapp/dtos/ProductDto.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
 
 @Data
