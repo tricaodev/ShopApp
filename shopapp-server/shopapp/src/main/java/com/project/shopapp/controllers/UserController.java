b378eca0127fe6765d5ddfa0b02c8d4966f630f8
Tri Cao <tricao.dev@gmail.com>
2024-05-13 22:54:41 +0700

    Các method với UserController

---
+++ shopapp-server/shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -0,0 +1,40 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.UserDto;
+import com.project.shopapp.dtos.UserLoginDto;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserDto user, BindingResult result) {
+        try {
+            if(result.hasErrors()) {
+                List<String> errorMessage = result.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+                return ResponseEntity.badRequest().body(errorMessage);
+            }
+            if(!user.getPassword().equals(user.getRetypePassword())) {
+                return ResponseEntity.badRequest().body("Password does not match");
+            }
+            return ResponseEntity.ok("Register successfully");
+        }
+        catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDto userLogin) {
+        return ResponseEntity.ok("Login successfully");
+    }
+}
