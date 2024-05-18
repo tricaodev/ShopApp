11bf53bff35702f917721264b3b39c62b6dca4b0
Tri Cao <tricao.dev@gmail.com>
2024-05-18 22:50:33 +0700

    Model cho thực thể Order và OrderDetail

---
+++ shopapp-server/shopapp/src/main/java/com/project/shopapp/models/Order.java
@@ -0,0 +1,60 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "fullname", length = 100)
+    private String fullName;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 200)
+    private String address;
+
+    @Column(length = 100)
+    private String note;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    private OrderStatus status;
+
+    @Column(name = "total_money")
+    private Float totalMoney;
+
+    @Column(name = "shipping_method", nullable = false, length = 100)
+    private String shippingMethod;
+
+    @Column(name = "shipping_address", nullable = false, length = 200)
+    private String shippingAddress;
+
+    @Column(name = "shipping_date", nullable = false)
+    private Date shippingDate;
+
+    @Column(name = "tracking_number", nullable = false, length = 100)
+    private String trackingNumber;
+
+    @Column(name = "payment_method", nullable = false, length = 100)
+    private String paymentMethod;
+
+    private Boolean active;
+}
