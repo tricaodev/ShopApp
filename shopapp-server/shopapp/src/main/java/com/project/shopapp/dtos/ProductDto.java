e459ab115b5ad754d2026cbdda996f43e5849422
Tri Cao <tricao.dev@gmail.com>
2024-06-11 09:44:22 +0700

    refactor code

--- shopapp-server/shopapp/src/main/java/com/project/shopapp/dtos/ProductDto.java
+++ shopapp-server/shopapp/src/main/java/com/project/shopapp/dtos/ProductDto.java
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
 
 @Data
 public class ProductDto {
